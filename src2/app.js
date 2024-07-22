547d21230a2f859b997d8aace9a32db23751ed4e
sebastianMeguillanes <sebastian.meguillanes@ucb.edu.bo>
2024-07-22 19:20:33 -0400

    prueba de carga

--- src2/app.js
+++ src2/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const guard = require('./utils/authMiddleware');
-const path = require('path');
 
 const app = express();
 app.use(cors());
@@ -16,8 +15,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/urls',guard.authMiddleware, urlRoutes);
 app.use('/api/versions',guard.authMiddleware, versionRoutes);   
 app.use('/loaderio-b9fcbe9ac99c68d1f3215e762115ff34', (req, res) => {
-    res.sendFile(path.join(__dirname, 'loaderio-b9fcbe9ac99c68d1f3215e762115ff34'));
-  });
+
+    //res.sendFile(path.join(__dirname, 'loaderio-b9fcbe9ac99c68d1f3215e762115ff34.txt'));
+    res.status(200).json( "loaderio-b9fcbe9ac99c68d1f3215e762115ff34" );
+});
 
 
 module.exports = app;
